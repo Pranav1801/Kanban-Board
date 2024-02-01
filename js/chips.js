2c071a4cbd172a1f1bcc6de8953cb0a813158810
Pranav Gupta <pranavgupta1801@gmail.com>
2024-02-02 01:31:10 +0530

    Optimized chips.js for common use.

--- js/chips.js
+++ js/chips.js
@@ -1,35 +1,43 @@
-var input = document.querySelector(".chip-input");
-var chips = document.querySelector(".chips");
+window.onload = () => addChip();
 
-input.addEventListener('keypress', (e) => {
-	if(e.which == 13){
-		var chip = document.createElement('div');
-		chip.classList.add('chip');
-		chip.addEventListener('click', chipClickHandler);
+const addChip = () => {
+	var input = document.querySelector(".chip-input");
+	input.addEventListener('keypress', (e) => {
+		if(e.which == 13){
+			createChip(e.target.value);
+			console.log('kjabbkwa', e.target.value)
+			e.target.value = "";
+		}
+	})
+}
 
-		var chip_text = document.createElement('span');
-		chip_text.classList.add('chip--text');
-		chip_text.innerHTML = input.value;
+const createChip = (inputValue) => {
+	var chips = document.querySelector(".chips");
+	var chip = document.createElement('div');
+	chip.classList.add('chip');
+	chip.addEventListener('click', chipClickHandler);
 
-		var chip_button = document.createElement('span');
-		chip_button.classList.add('chip--button');
-		chip_button.innerHTML = 'X';
+	var chip_text = document.createElement('span');
+	chip_text.classList.add('chip--text');
+	chip_text.innerHTML = inputValue;
 
-		var deleteIcon = document.createElement('img');
-		deleteIcon.src = '/assets/cross_icon.svg';
-		deleteIcon.style.fill = 'red';
-		deleteIcon.width = 10;
-		
-		// chip_button.append(deleteIcon);
+	var chip_button = document.createElement('span');
+	chip_button.classList.add('chip--button');
+	chip_button.innerHTML = 'X';
 
-		chip.appendChild(chip_text);
-		chip.appendChild(chip_button);
-		chips.appendChild(chip);
+	var deleteIcon = document.createElement('img');
+	deleteIcon.src = '/assets/cross_icon.svg';
+	deleteIcon.style.fill = 'red';
+	deleteIcon.width = 10;
+	
+	// chip_button.append(deleteIcon);
 
-		input.value = '';
-	}
-})
-  
+	chip.appendChild(chip_text);
+	chip.appendChild(chip_button);
+	chips.appendChild(chip);
+
+}
+
 function chipClickHandler(event){
 	chips.removeChild(event.currentTarget);
 }
