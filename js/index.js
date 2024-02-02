147ca1af58e4cf7844b83b8fbb17d0cebe17e7c7
Pranav Gupta <pranavgupta1801@gmail.com>
2024-02-03 01:23:48 +0530

    Added tags in cards in index.html.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+window.onload = fetchTasks();
+
 const fetchTasks = () =>{
     const tasksJSON = localStorage.getItem('tasks_json');
     const todoTasks = document.querySelector('#todo-tasks');
@@ -11,8 +13,6 @@ const fetchTasks = () =>{
         todoTasks.appendChild(emptyText);
         inProgressTasks.appendChild(emptyText.cloneNode(true));
         doneTasks.appendChild(emptyText.cloneNode(true));
-        // tasks.appendChild(emptyText.cloneNode(true));
-        // tasks.appendChild(emptyText.cloneNode(true));
     } else {
 
         JSON.parse(tasksJSON).forEach(task => {
@@ -23,31 +23,67 @@ const fetchTasks = () =>{
             titleDiv.style.display = "flex";
             titleDiv.style.flexDirection = "row";
             titleDiv.style.justifyContent = "space-between";
+            titleDiv.style.paddingBottom = '10px';
 
-    
             const title = document.createElement('label');
             title.innerHTML = task.todo;
-            title.style.fontSize = "30px";
+            title.style.fontSize = "20px";
+            title.style.fontWeight = 'bold';
 
             const editIcon = document.createElement('img');
             editIcon.src = '/assets/edit_icon.svg';
 
+            const chipDiv = document.createElement('div');
+            chipDiv.classList.add('chips');
+            chipDiv.style.paddingBottom = "10px";
+            
+            if(task.tags != ""){
+                task.tags.forEach(tag =>{
+                    const chip = getTags(tag)
+                    chipDiv.append(chip);
+                })
+            }
+
             const desc = document.createElement('label');
             desc.innerHTML = task.description;
 
-            const createTime = document.createElement('label');
+            const footerDiv = document.createElement('div');
+            footerDiv.style.display = "flex";
+            footerDiv.style.flexDirection = "row";
+            footerDiv.style.justifyContent = "space-between";
+            footerDiv.style.paddingTop = "30px"
+
             const time = new Date(task.created);
-            createTime.innerHTML = time.toLocaleDateString() + " " + time.toLocaleTimeString();
+            const createTime = document.createElement('label');
+            createTime.innerHTML = "Created At: " + time.toLocaleDateString() + " " + time.toLocaleTimeString();
+            createTime.style.fontSize = "15px";
+
+            const update = new Date(task.updated);
+            const updateTime = document.createElement('label');
+            updateTime.innerHTML = "Updated At: " + update.toLocaleDateString() + " " + update.toLocaleTimeString();
+            updateTime.style.fontSize = "15px";
 
             titleDiv.appendChild(title);
             titleDiv.appendChild(editIcon);
 
+            footerDiv.appendChild(createTime);
+            footerDiv.appendChild(updateTime);
 
             card.appendChild(titleDiv);
+            card.appendChild(chipDiv);
             card.appendChild(desc);
-            card.appendChild(createTime);
+            card.appendChild(footerDiv);
 
             if(task.status == "Todo") todoTasks.appendChild(card);
+            else if(task.status == "In Progress") {
+                card.style.backgroundColor = "#584666";
+                inProgressTasks.appendChild(card);
+                titleDiv.removeChild(editIcon);
+            } else if(task.status == "Done") {
+                card.style.backgroundColor = "#4E6646";
+                doneTasks.appendChild(card)
+                titleDiv.removeChild(editIcon);
+            }
 
             editIcon.addEventListener("click", function(){ 
                 window.open("/views/edit.html?timestamp="+task.created, "_self");
@@ -57,5 +93,27 @@ const fetchTasks = () =>{
     }
 }
 
+const getTags = (inputValue) => {
+    var chip = document.createElement('div');
+    chip.classList.add('chip');
+    chip.style.backgroundColor = generateRandomColor();
+    chip.style.height = '30px';
 
-fetchTasks();
+    var chip_text = document.createElement('span');
+    chip_text.classList.add('chip--text');
+    chip_text.innerHTML = inputValue;
+
+    var chip_button = document.createElement('span');
+    chip_button.classList.add('chip--button');
+    chip_button.innerHTML = 'X';
+    
+    chip.appendChild(chip_text);
+    generateRandomColor();
+    return chip;
+}
+
+const generateRandomColor = () => {
+    const chipColors = ['#59BEDE', '#C4DE59']
+    const temp = chipColors[Math.floor(Math.random() * 2)];
+    return temp;
+}
